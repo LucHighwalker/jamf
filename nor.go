cdaa45abfb6a2e757e0cc706b3cddbdd2df84218
LucHighwalker <luc.highwalker@gmail.com>
2020-01-23 15:52:11 -0800

    Basic cli setup.

--- nor.go
+++ nor.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+var app = cli.NewApp()
+
+func info() {
+	app.Name = "Node on Rails"
+	app.Usage = "Like Ruby on Rails but NodeJS"
+	author := cli.Author{Name: "Luc Highwalker", Email: "email@luc.gg"}
+	app.Authors = []*cli.Author{&author}
+	app.Version = "0.0.1"
+}
+
+func main() {
+	info()
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
