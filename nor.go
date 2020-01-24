9b109e6213d4bb2465e6de1d57b5185df699e7d7
LucHighwalker <luc.highwalker@gmail.com>
2020-01-23 16:23:34 -0800

    Planned out commands.

--- nor.go
+++ nor.go
@@ -17,8 +17,59 @@ func info() {
 	app.Version = "0.0.1"
 }
 
+func commands() {
+	app.Commands = []*cli.Command{
+		{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   "Initialize a new NoR project.",
+			Action: func(c *cli.Context) error {
+				// initialize
+				return nil
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "Add an existing module.",
+			Action: func(c *cli.Context) error {
+				// add module
+				return nil
+			},
+		},
+		{
+			Name:    "controller",
+			Aliases: []string{"c"},
+			Usage:   "Create a new controller.",
+			Action: func(c *cli.Context) error {
+				// generate controller
+				return nil
+			},
+		},
+		{
+			Name:    "model",
+			Aliases: []string{"m"},
+			Usage:   "Generate a model.",
+			Action: func(c *cli.Context) error {
+				// Generate model
+				return nil
+			},
+		},
+		{
+			Name:    "struct",
+			Aliases: []string{"s"},
+			Usage:   "Generate a structure (model/controller).",
+			Action: func(c *cli.Context) error {
+				// Generate a model struct
+				return nil
+			},
+		},
+	}
+}
+
 func main() {
 	info()
+	commands()
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
